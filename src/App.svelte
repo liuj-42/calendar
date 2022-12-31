f9c158a84b98d9998e4e14b80e4846376c75bfaf
James Liu <liuxjames88@gmail.com>
2022-12-31 14:55:23 -0500

    creating outline

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+    import Tasks from "./lib/tasks.svelte";
+</script>
+
+<main>
+  <div id="tasks">
+    <Tasks />
+  </div>
+  <div id="today">
+    <h2>Today</h2>
+    <div id="today-events">
+      <div class="event">
+        <h3>Event 1</h3>
+        <p>Event 1 description</p>
+      </div>
+      <div class="event">
+        <h3>Event 2</h3>
+        <p>Event 2 description</p>
+      </div>
+      <div class="event">
+        <h3>Event 3</h3>
+        <p>Event 3 description</p>
+      </div>
+    </div>
+  </div>
+  <div id="view">
+    <h2>weekly/monthly view here</h2>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+  }
+
+  #tasks, #today, #view {
+    border: 1px solid fuchsia;
+  }
+  
+  #tasks {
+    flex-basis: 20%;
+  }
+  #today {
+    flex-basis: 30%;
+  }
+  #view {
+    flex-basis: 50%;
+  }
+</style>
---
+++ src/lib/tasks.svelte
@@ -0,0 +1,20 @@
+<script>
+    // your script goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<main>
+    <h2>Tasks</h2>
+    <ul>
+        <li>task 1</li>
+        <li>task 2</li>
+        <li>task 3</li>
+    </ul>
+</main>
+
+<style scoped>
+    /* your styles go here */
+    main {
+        
+    }
+</style>
